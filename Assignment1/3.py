237f5ec95aae3418347c26451b8f1d4742502362
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-08-19 15:09:51 +0530

    Add detailed comments and sample test cases for vowel counting script

--- Assignment1/3.py
+++ Assignment1/3.py
@@ -1,3 +1,19 @@
-# 
+# 3.	While analysing intercepted communications, TASC officer Srikant Tiwari suspects that some messages might be carrying hidden signals. One of the red flags used by the agency is the density of vowels in a message — unusually high vowel counts may indicate a coded alert.
+# To assist in screening these messages quickly, a script is needed to count the number of vowels in any given string.
+# Write a Python program that:
+# Takes a message as input.
+# Counts the number of vowels (a, e, i, o, u — both uppercase and lowercase).
+# Prints the vowel count.
+# Input Format:
+# A single line containing a string .
+# Output Format:
+# A single integer — the count of vowels (A, E, I, O, U in both uppercase and lowercase).
+# Sample Test Cases
 
-
+# 	Input	Output
+# Test Case 1	Data Science	5
+# Test Case 2	qwrtypsdfghj	0
+# Test Case 3	I love programming!	6
+# Test Case 4	hello	2
+# Test Case 5	PYTHON	1
+# Test Case 6	OpenAI 123	4
