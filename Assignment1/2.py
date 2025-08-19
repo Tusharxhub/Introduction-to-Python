00a66ea1aa5681c113a9ee40bbe16a772b5cfe06
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-08-19 15:02:30 +0530

    Add description and sample test cases for Neon Number validation

---
+++ Assignment1/2.py
@@ -0,0 +1,13 @@
+# 2.	A high-security vault uses a custom PIN authentication mechanism. To reduce the chances of brute-force attacks, the system only allows PINs that are Neon Numbers.
+# The rule is:
+# A PIN is valid if the sum of the digits of its square equals the PIN itself.
+# Your Task: Write a Python program that reads a numeric PIN input by the user and validates whether it is a Neon Number or not.
+# Sample Test Cases
+
+# 	Input	Output
+# Test Case 1	9	Neon Number
+# Test Case 2	12	Not Neon Number
+# Test Case 3	10	Not Neon Number
+# Test Case 4	0	Neon Number
+# Test Case 5	1	Neon Number
+# Test Case 6	2	Not Neon Number
