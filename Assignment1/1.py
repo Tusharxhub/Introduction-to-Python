c0716dda2687d130ba10d035ec21757d41a2644e
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-08-19 14:50:50 +0530

    Add initial implementation for calculating the sum of odd integers up to n

---
+++ Assignment1/1.py
@@ -0,0 +1,21 @@
+# 1.	Gautam Gambhir, freshly appointed head coach for India’s 2025 away Test series against England, has asked the new analyst, Aryan, to keep a quirky statistic:
+# “At the end of each day’s play,” Gambhir says, “tell me the total runs we scored in every odd-numbered over—1st, 3rd, 5th … all the way up to the last over bowled that day. It helps me spot momentum in the sessions where bowlers are freshest.”
+# Aryan realises the job boils down to simple maths: if n is the last over of the day (always a positive integer), he just needs the sum of all odd integers from 1 through n.
+# Your task is to step into Aryan’s shoes and automate this little ritual.
+# Input Format:
+# A single integer
+# Output Format:
+# A single integer: the sum of all odd numbers from 1 to n
+# Sample Test Cases
+
+# 	Input	Output
+# Test Case 1	15	64
+# Test Case 2	100	2500
+# Test Case 3	9	25
+# Test Case 4	1	1
+# Test Case 5	2	1
+# Test Case 6	10	25
+
+
+
+
