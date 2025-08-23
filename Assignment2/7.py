142085225532c722cf9f0ec39d5613e99d42c200
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-08-23 10:32:03 +0530

    Add JSON data reader and updater for user information

---
+++ Assignment2/7.py
@@ -0,0 +1,3 @@
+# Create a program that reads data from a JSON file containing user information, modifies a specific entry (e.g., changes a user's email), and saves the updated data back to a new JSON file.
+
+
