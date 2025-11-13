bc2489728194348fd5dc22f113586b1259cd1d60
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-11-13 15:51:53 +0530

    Update hint in Fourier Transform program to specify scipy.fft.fft()

--- Assignment4/4.py
+++ Assignment4/4.py
@@ -1,1 +1,2 @@
-#Write a Python program using SciPy to find the Fourier Transform of a 1D signal [1, 2, 1, 0].
+#Write a Python program using SciPy to find the Fourier Transform of a 1D signal [1, 2, 1, 0].(Hint: Use scipy.fft.fft()).
+
