cd61b60ac6a04c3adfa134e5bc487110bda2e0b9
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-11-13 15:49:09 +0530

    Implement matrix multiplication program using NumPy

--- Assignment4/3.py
+++ Assignment4/3.py
@@ -1,2 +1,15 @@
 #Write a program to perform matrix multiplication using NumPy.(Hint: Use np.dot() or @ operator).
+import numpy as np
+# Define two matrices
+matrix_a = np.array([[1, 2, 3],
+                        [4, 5, 6]])
+matrix_b = np.array([[7, 8],                   
+                        [9, 10],
+                        [11, 12]])
+# Perform matrix multiplication
+result = np.dot(matrix_a, matrix_b)
+# Alternatively, you can use the @ operator
+# result = matrix_a @ matrix_b
+# Display the result
+print(result)
 
