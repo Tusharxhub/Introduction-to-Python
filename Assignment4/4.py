4faf9fbaf255fbf94f746a8d2005d322702b0821
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-11-13 15:59:19 +0530

    Add Python program to compute Fourier Transform of a 1D signal using SciPy

--- Assignment4/4.py
+++ Assignment4/4.py
@@ -1,2 +1,10 @@
 #Write a Python program using SciPy to find the Fourier Transform of a 1D signal [1, 2, 1, 0].(Hint: Use scipy.fft.fft()).
 
+import numpy as np
+from scipy.fft import fft
+
+signal = np.array([1, 2, 1, 0], dtype=float)
+spectrum = fft(signal)
+
+print("Signal:", signal)
+print("Fourier Transform:", spectrum)
